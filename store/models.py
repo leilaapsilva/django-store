22d3b8270f237cc4974393c5aa30ca67172d6710
Leila <leilaapsilva26@gmail.com>
2020-07-14 22:52:15 -0300

    Initial commit with files

---
+++ store/models.py
@@ -0,0 +1,17 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField(default=0)
+    creation_date = models.DateTimeField('creation date', null=True)
+
+    def __str__(self):
+        return self.name
+
+    def was_created_recently(self):
+        return self.creation_date >= timezone.now() - datetime.timedelta(days=1)    
