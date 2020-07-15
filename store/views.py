e902920f8adb13fbd18286ba227a9b80fb99dd83
Leila <leilaapsilva26@gmail.com>
2020-07-15 15:17:46 -0300

    Create simple list of latest products

--- store/views.py
+++ store/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import Product
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello world, this is the store model")
+
+    #returns a list of the 5 latest products
+    latest_products_list = Product.objects.order_by('-creation_date')[:5]
+    #template = loader.get_template('store/index.html')
+    context = {
+        'latest_products_list': latest_products_list,
+    }
+    
+    #output = ', '.join([p.name for p in latest_products_list])
+
+    return render(request, 'store/index.html', context)
+
+#information about product
+def detail(request, product_id):
+    return HttpResponse("You're looking at product %s." % product_id)    
+
