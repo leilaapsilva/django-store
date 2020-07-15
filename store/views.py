277a86aefe259ff77aa70ecf247b5190488d6d1e
Leila <leilaapsilva26@gmail.com>
2020-07-15 15:39:58 -0300

    Handle 404 error

--- store/views.py
+++ store/views.py
@@ -1,22 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
 from .models import Product
-from django.template import loader
 
 def index(request):
 
     #returns a list of the 5 latest products
     latest_products_list = Product.objects.order_by('-creation_date')[:5]
-    #template = loader.get_template('store/index.html')
     context = {
         'latest_products_list': latest_products_list,
     }
     
-    #output = ', '.join([p.name for p in latest_products_list])
-
     return render(request, 'store/index.html', context)
 
 #information about product
 def detail(request, product_id):
-    return HttpResponse("You're looking at product %s." % product_id)    
+    #404 error if product does not exist
+    product = get_object_or_404(Product, id=product_id)     
+
+    return render(request, 'store/detail.html', {'product': product})    
 
